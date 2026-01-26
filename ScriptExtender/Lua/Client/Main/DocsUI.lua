f7fe375a99904f917399e5ebbd3bdbad5e9dfb9c
rakor2 <winster2207@gmail.com>
2026-01-26 19:49:13 +0300

    Add Docs tab

---
+++ ScriptExtender/Lua/Client/Main/DocsUI.lua
@@ -0,0 +1,6 @@
+function Docs2Tab(parent)
+    local p = parent
+    p:AddText([[For the documentation download MazzleDocs
+and place it above Lighty Lights in your load order.
+Then you can acces it by opening MazzleDocs' UI]])
+end
